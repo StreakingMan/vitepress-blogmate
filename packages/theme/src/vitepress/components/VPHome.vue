28e66fe97c6bf1a4d42df9a1b6e37f94109703d8
streakingman <z_max_y@163.com>
2023-08-22 09:23:46 +0800

    chore: 首页源码复制

--- packages/theme/src/vitepress/components/VPHome.vue
+++ packages/theme/src/vitepress/components/VPHome.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
-//
+import VPHomeHero from 'vitepress/dist/client/theme-default/components/VPHomeHero.vue';
+import VPHomeFeatures from 'vitepress/dist/client/theme-default/components/VPHomeFeatures.vue';
 </script>
 
 <template>
-    <div>This is Home 2</div>
+    <div class="VPHome">
+        <slot name="home-hero-before" />
+        <VPHomeHero>
+            <template #home-hero-info><slot name="home-hero-info" /></template>
+            <template #home-hero-image>
+                <slot name="home-hero-image" />
+            </template>
+        </VPHomeHero>
+        <slot name="home-hero-after" />
+
+        <slot name="home-features-before" />
+        <VPHomeFeatures />
+        <slot name="home-features-after" />
+
+        <Content />
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.VPHome {
+    padding-bottom: 96px;
+}
+
+.VPHome :deep(.VPHomeSponsors) {
+    margin-top: 112px;
+    margin-bottom: -128px;
+}
+
+@media (min-width: 768px) {
+    .VPHome {
+        padding-bottom: 128px;
+    }
+}
+</style>
