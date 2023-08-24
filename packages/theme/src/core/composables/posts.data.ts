4f2e43496852d897eca04f26a2a68d12750eb3ca
streakingman <z_max_y@163.com>
2023-08-24 14:06:43 +0800

    feat: 拓展posts.data.ts 增加更新时间、标题、阅读时间信息

--- packages/theme/src/core/composables/posts.data.ts
+++ packages/theme/src/core/composables/posts.data.ts
@@ -1,7 +1,46 @@
 import { createContentLoader } from 'vitepress';
 import type { ContentData } from 'vitepress';
+import { getGitTimestamp } from '../utils/getGitTimestamp';
 
-declare const data: ContentData[];
+export type ContentDataExtra = ContentData & {
+    lastUpdated: number;
+    title: string;
+    readingTime: number;
+};
+
+declare const data: ContentDataExtra[];
 export { data };
 
-export default createContentLoader('*.md' /* options */);
+export default createContentLoader<ContentDataExtra[]>('posts/*.md', {
+    includeSrc: true,
+    excerpt(file) {
+        file.excerpt = file.content
+            .split('\n')
+            .filter((c) => !!c.trim() && !c.startsWith('# '))
+            .slice(0, 4)
+            .join(' ');
+    },
+    async transform(rawData) {
+        const _rawData: ContentDataExtra[] = [];
+        for (const file of rawData) {
+            const lastUpdated = await getGitTimestamp(file.url.slice(1).replace(/\.html$/, '.md'));
+            let title = '',
+                readingTime = 0;
+            if (file.src) {
+                // 匹配出现的第一行一级标题
+                title = file.src.match(/^#\s+(.*)/m)?.[1] || '';
+                // 阅读时间，按照 200 字/分钟计算
+                readingTime = Math.ceil(file.src.length / 200);
+            }
+            _rawData.push({
+                ...file,
+                lastUpdated,
+                title,
+                readingTime,
+            });
+        }
+        return _rawData.sort((a, b) => {
+            return b.lastUpdated - a.lastUpdated;
+        });
+    },
+});
