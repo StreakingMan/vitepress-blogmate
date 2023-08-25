743197ffff7388840741dfb869a3e81dd76ff368
streakingman <z_max_y@163.com>
2023-08-25 18:00:06 +0800

    feat: 标签文章过滤

--- packages/theme/src/core/components/BTTags.vue
+++ packages/theme/src/core/components/BTTags.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { usePosts } from '../composables/usePosts';
 import { computed, ref } from 'vue';
+import { useElementSize, useUrlSearchParams } from '@vueuse/core';
+import BTPostCard from './BTPostCard.vue';
 
 const posts = usePosts();
 
@@ -20,26 +22,68 @@ const search = ref('');
 const searchedTags = computed(() => {
     return tags.filter((t) => t.includes(search.value));
 });
+
+const boxEl = ref(null);
+const { height } = useElementSize(boxEl);
+
+const activeTag = ref('');
+const { tag: queryTag } = useUrlSearchParams('history');
+if (queryTag) {
+    const _tag = Array.isArray(queryTag) ? queryTag[0] : queryTag;
+    if (tags.includes(_tag)) {
+        activeTag.value = _tag;
+    }
+}
+const onTagClick = (tag: string) => {
+    if (activeTag.value === tag) {
+        activeTag.value = '';
+    } else {
+        activeTag.value = tag;
+    }
+};
+const filteredPosts = computed(() => {
+    if (!activeTag.value) {
+        return posts;
+    }
+    return posts.filter((p) => p.frontmatter.tags.includes(activeTag.value));
+});
 </script>
 
 <template>
     <div class="bt-tags-container">
         <input v-model="search" placeholder="搜索标签" class="bt-tags-input" />
-        <div class="bt-tags-box-wrapper">
-            <div class="bt-tags-box">
-                <span v-if="searchedTags.length === 0" class="bt-tags-item">啥也没有</span>
+        <div
+            class="bt-tags-box-wrapper"
+            :style="{
+                height: `${height}px`,
+            }"
+        >
+            <div ref="boxEl" class="bt-tags-box">
+                <span
+                    v-if="searchedTags.length === 0"
+                    class="bt-tags-item"
+                    style="font-size: 1.2em"
+                >
+                    啥也没有~
+                </span>
                 <a
                     v-for="tag in searchedTags"
                     :key="tag"
                     class="bt-tags-item"
+                    :class="{
+                        active: activeTag === tag,
+                    }"
                     :style="{
                         fontSize: `${1.2 + (tagsMap.get(tag) || 0) * 0.6}em`,
                         animationDelay: `${Math.random() * 0.5}s`,
                     }"
+                    @click="onTagClick(tag)"
                 >
                     #{{ tag }}
                 </a>
             </div>
         </div>
+
+        <BTPostCard v-for="post in filteredPosts" v-bind="post" :key="post.url" />
     </div>
 </template>
